993b44a20ea20a76ee189cd35f69583c3a6ed3ad
onKTun <kevinkltun@gmail.com>
2024-07-18 17:53:01 -0700

    video layer, ml model, lilys commits

---
+++ HackTheMoose - Exercise App/SplashScreenView.swift
@@ -0,0 +1,54 @@
+//
+//  SplashScreenView.swift
+//  Proactive
+//
+//  Created by lily on 7/14/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State var isActive : Bool = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    // Customise your SplashScreen here
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack(spacing:-100) {
+                    Image(.logo)
+                        .font(.system(size: 80))
+                        .foregroundColor(.red)
+                            
+                    Text("Proactive")
+                        .font(.system(size:50, design: .monospaced))
+                        .foregroundColor(.black.opacity(0.80))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.00
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
